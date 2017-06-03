0cff23137f72a8fb8eed72486f7478402016a0fb
signigni <illlyapolll@gmail.com>
2017-06-03 23:11:03 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main() {
+    srand(time(NULL));
+    char *word = new char[10];
+    short key[10];
+
+    for (int i = 0; i < 10; i++) {
+        key[i] = rand() % 128 - 127;
+        cin >> word[i];
+        word[i] = word[i] ^ key[i];
+    }
+
+    for (int i = 0; i < 10; i++)
+        cout << word[i];
+    cout << endl;
+
+    for (int i = 0; i < 10; i++) {
+        word[i] = word[i] ^ key[i];
+        cout << word[i];
+    }
+    cout << endl;
+
+    return 0;
+}
